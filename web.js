c2b4be399a6f98697d23cf7cb84734783a63fe22
Priyanka Kerhalkar <mangalath.priyanka@gmail.com>
2013-07-28 14:08:12 +0000

    bug fix

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+var fs = require('fs');
 var buffer = new Buffer(fs.readFileSync('index.html','utf-8'), "utf-8");
   response.send(buffer.toString());
 });
